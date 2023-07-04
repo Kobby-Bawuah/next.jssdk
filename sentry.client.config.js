037575374d6429944dc322acfe8f9b5104040d47
KobbyBawuah <Bawuah10@hotmail.com>
2023-07-04 17:20:02 -0400

    working on conflicts

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -4,6 +4,7 @@
 
 import * as Sentry from '@sentry/nextjs';
 // import * as Sentry from '@sentry/browser';
+import { RewriteFrames } from "@sentry/integrations";
 
 
 
@@ -12,7 +13,7 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN;
 Sentry.init({
   dsn: SENTRY_DSN || 'https://eea52d86a96740cb925b76ec56f894e2@o1145044.ingest.sentry.io/4504053104312320',
   // Adjust this value in production, or use tracesSampler for greater control
-  
+
   beforeSend(event) {
     // Modify or drop the event here
     // culprit="my_custom_culprit"
@@ -27,22 +28,30 @@ Sentry.init({
   replaysOnErrorSampleRate: 1.0,
 
   // release: "12",
-  release: "9.2.2",
+  release: "9.1.1",
   integrations: [
     new Sentry.BrowserTracing({
       tracingOrigins: ["localhost", /^\//, /.*\.convex.cloud/],
-      }), 
-    // new Sentry.Replay({
-    //   // Additional SDK configuration goes in here, for example:
-    //   maskAllText: true,
-    //   blockAllMedia: true,
+    }),
+
+    // new RewriteFrames({
+    //   iteratee: (frame) => {
+    //     frame.filename = "app:///" + frame.filename.split('/').slice(-2).join('/')
+    //     return frame
+    //   },
     // }),
+
+    new Sentry.Replay({
+      // Additional SDK configuration goes in here, for example:
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
   ],
-  integrations: [new Sentry.Replay({
-    maskAllText: false,
-    blockAllMedia: false,
-  })
-], 
+  // integrations: [new Sentry.Replay({
+  //   maskAllText: false,
+  //   blockAllMedia: false,
+  // })
+  // ],
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
@@ -66,7 +75,15 @@ const currentUserInfo = {
   id: 45663,
   email: "test.doe@example.com"
 }
-Sentry.setUser({ id: currentUserInfo?.id, email: currentUserInfo?.email }); 
+Sentry.setUser({ id: currentUserInfo?.id, email: currentUserInfo?.email });
+
+Sentry.setContext("character", {
+  name: "Mighty Fighter",
+  age: 19,
+  attack_type: "melee",
+  event: "auth",
+  password: "rahh"
+});
 
 // Sentry.setUser({ email: "fran.doe@example.com", username: "Bigfran" });
 
